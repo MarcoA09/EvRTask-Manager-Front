13ce9b90213db1440ad8f3c2bc2373dc2420163f
MarcoA09 <marcoa.marquez06@gmail.com>
2025-03-08 20:14:08 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-  base: '', // Intenta sin './' para evitar problemas con los estilos
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
   },
   server: {
-    historyApiFallback: true, // Asegura que las rutas de React funcionen correctamente
+    historyApiFallback: true, 
   },
 });
