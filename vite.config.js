e5f9d6b945bd7185c2b98f0fc4eaa61bd0d1c604
MarcoA09 <marcoa.marquez06@gmail.com>
2025-03-08 20:04:01 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()], 
-    },
-  },
   base: './', 
   build: {
     outDir: 'dist',
